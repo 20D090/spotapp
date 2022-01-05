6277561520027bf816b89ec4676d6c52dd7bd0ee
20D090 <92702419+20D090@users.noreply.github.com>
2022-01-05 15:28:43 +0900

    Update env.js

--- assets/env.js
+++ assets/env.js
@@ -1,2 +1,2 @@
-SSID=https://github.com/20D090/spotapp/edit/main/assets/env.js
+SSID=https://sheets.googleapis.com/v4/spreadsheets/1ftvJU7wplXHh71JG4T3bxIj7ZlIzRzeD8TrynC_obtQ/values/okazaki!A2:K1000?key=AIzaSyD_IOHRq23QuvGfO-N_LYKDyDs9-27v1ek
 SNAME=okazaki!A2:K1000
